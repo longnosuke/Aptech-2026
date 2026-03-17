6220d9752074e25416c8ef842a9e4ea0a23ebe2f
Thanh Long <thanhlongnosuke@gmail.com>
2026-03-17 18:06:28 +0700

    Update a clean code version

--- leapyear.c
+++ leapyear.c
@@ -1,18 +1,31 @@
 #include <stdio.h>
+// int main() {
+//   int year;
+//   printf("Enter a year: ");
+//   scanf( "%d", &year );
+//   if ( year % 4 == 0 ) {
+//     if ( year % 100 == 0 ) {
+//       if ( year % 400 == 0 ) {
+//         printf( "%d is a leap year.\n", year );
+//       } else {
+//         printf( "%d is not a leap year.\n", year );
+//       }
+//     } else {
+//       printf( "%d is a leap year.\n", year );
+//     }
+//   } else {
+//     printf( "%d is not a leap year.\n", year );
+//   }
+//   return 0;
+// }
+
+// More optimal way to code
 int main() {
   int year;
-  printf("Enter a year: ");
-  scanf( "%d", &year );
-  if ( year % 4 == 0 ) {
-    if ( year % 100 == 0 ) {
-      if ( year % 400 == 0 ) {
-        printf( "%d is a leap year.\n", year );
-      } else {
-        printf( "%d is not a leap year.\n", year );
-      }
-    } else {
-      printf( "%d is a leap year.\n", year );
-    }
+  printf("Please enter the year: ");
+  scanf( "%d", &year);
+  if ( year % 4 == 0 && year % 100 != 0 || year % 400 == 0 ) {
+    printf( "%d is a leap year.\n", year );
   } else {
     printf( "%d is not a leap year.\n", year );
   }
